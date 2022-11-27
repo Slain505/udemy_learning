0f3eeb38ef418182e2396fec25db3bf22e366ef9
Slain505 <vladyslav.yefimenko@protonmail.com>
2022-11-27 15:18:06 +0100

    commit

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;   // Create speed variable 
+    public float jumpForce; // Create jump variable 
+    public Rigidbody2D _rb; // Create variable RB 2D
+    private bool isGrounded; // Create variable which checks if Player on ground
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rb.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), _rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle()
+        if (Input.GetButtonDown("Jump")) {
+            
+            if (isGrounded){
+
+            
+            _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
+            }
+        }
+    }
+}
